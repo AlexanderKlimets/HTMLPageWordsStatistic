057f7f18c3564aa9e4cceb9fa0dacd668b5ea06f
Alexander <sashaklimets@list.ru>
2021-07-05 19:07:56 +0300

    Downloading HTML code from WebPage
    Receiving HTML code for a given URL as string variable

---
+++ HTMLPageWordStatistic/Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Net;
+
+namespace HTMLPageWordStatistic
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            using (WebClient client = new WebClient())
+            {
+                string htmlCode = client.DownloadString("https://www.simbirsoft.com/");
+            }
+        }
+    }
+}
