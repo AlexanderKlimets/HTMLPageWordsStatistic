7b425f3819459881c9a5772cfcf7bb2799e24c39
Alexander <sashaklimets@list.ru>
2021-07-05 20:12:40 +0300

    Implemented task in Program.cs
    Implemented loading HTML code from a Web page, splitting by a list of splitters, placing it in a dictionary with frequency counting and displaying it in the console

--- HTMLPageWordStatistic/Program.cs
+++ HTMLPageWordStatistic/Program.cs
@@ -4,6 +4,7 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 using System.Net;
+using HtmlAgilityPack;
 
 namespace HTMLPageWordStatistic
 {
@@ -15,6 +16,26 @@ namespace HTMLPageWordStatistic
             {
                 string htmlCode = client.DownloadString("https://www.simbirsoft.com/");
             }
+            HtmlWeb htmlWeb = new HtmlWeb();
+            HtmlAgilityPack.HtmlDocument document = htmlWeb.Load("https://www.simbirsoft.com/");
+            var htmlText = document.DocumentNode.InnerText;
+            var wordsCount = new Dictionary<string, int>();
+            foreach (var word in htmlText.Split
+               (' ', ',', '.', '!', '?', '"', ';', ':', '[', ']', '(', ')', '\n', '\r', '\t').Skip(1))
+            {
+                if (word == "")
+                { 
+                    continue; 
+                }
+                
+                wordsCount.TryGetValue(word.ToUpper(), out var count);
+                wordsCount[word.ToUpper()] = count + 1;
+            }
+            foreach (var str in wordsCount)
+            {
+                Console.WriteLine($"{str.Key}: {str.Value}");
+            }
+            Console.ReadLine();
         }
     }
 }
